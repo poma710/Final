3ae6da4b26501b6c2df55d60e0b1e27a74b08e56
Roman <poma710@yandex.run>
2023-06-05 21:16:43 +0300

    add prog

--- Program.cs
+++ Program.cs
@@ -2,17 +2,13 @@
 //  либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-public class Program { public static void Main() { Console.Write("Введите количество строк: "); int n = int.Parse(Console.ReadLine());
+using System;
 
-    string[] strings = new string[n]; // создание массива строк
-    for (int i = 0; i < n; i++) {
-        Console.Write($"Введите строку номер {i + 1}: ");
-        strings[i] = Console.ReadLine();
-    }
+public class Program { public static void Main() { string[] strings = {"abc", "defg", "hijklmn", "op", "qrs"}; 
 
-    string[] newStrings = new string[n]; // создание нового массива строк
-    int k = 0; // счетчик нового массива
-    for (int i = 0; i < n; i++) {
+    string[] newStrings = new string[strings.Length]; 
+    int k = 0; 
+    for (int i = 0; i < strings.Length; i++) {
         if (strings[i].Length <= 3) {
             newStrings[k] = strings[i];
             k++;
